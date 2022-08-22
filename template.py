1ce887e626c8412d88739d118f9725d5be979f31
SandeepKumar Hr <sandeepkumar.hr@lntinfotech.com>
2022-08-22 18:11:47 +0530

    dev env files

---
+++ template.py
@@ -0,0 +1,19 @@
+import os
+
+
+dirs = [ os.path.join('data','raw'),
+os.path.join('data','processed'),
+'notebooks',
+'saved_models','src','data_given']
+
+for i in dirs:
+    os.makedirs(i, exist_ok=True)
+    with open(os.path.join(i,".gitkeep"),'w') as f:
+        pass
+
+file_ = [
+    'dvc.yaml', 'params.yaml', '.gitignore', os.path.join('src', '__init__.py')]
+
+for i in file_:
+    with open(i,'w') as f:
+        pass
